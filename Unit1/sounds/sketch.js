e0387bac52608f897539962798ba7c08c6dc1597
ksnels2 <ksnels2@ilstu.edu>
2021-09-07 10:20:33 -0500

    mabey sound

---
+++ Unit1/sounds/sketch.js
@@ -0,0 +1,26 @@
+let song1;
+
+function preload() {
+  song1 = loadSound("assets/bensoundBetterdays.mp3");
+}
+
+function setup() {
+  createCanvas(500, 500);
+  song1.loop();
+}
+
+function draw() {
+  background("blue");
+}
+
+function mouseReleased() {
+  if {song1.isPlaying()) {
+    song1.pause();
+  } else {
+    song1.loop();
+  }
+}
+
+function touchStarted() {
+  getAudioContext().resume();
+}
