ce8892e03c132247f23a0a8ace6d5c2e3ee0ba1b
ksnels2 <ksnels2@ilstu.edu>
2021-09-08 20:21:36 -0500

    sa

--- Mp1/empty-example/sketch.js
+++ Mp1/empty-example/sketch.js
@@ -2,7 +2,8 @@ function setup() {
   createCanvas(1000, 800);
   rectMode(CENTER);
   ellipseMode(CENTER);
-
+    for(var i = 0; i < 200; i++)
+      drop[i] = new Drop();
 }
 
 
@@ -10,11 +11,62 @@ function setup() {
 function draw() {
 
   background(100);
+  function draw() {
+     background(220);
+    background("black");
+    fill("brown");
+    rect(200,200,200,100);
+    fill("blue");
+    ellipse(150,250,75,75);
+    ellipse(250,250,75,75);
+    line(400, 50 , 0 , 50);
+    line(400, 300 , 0 , 300);
+    line(400, 150 , 0 , 150);
+    fill ("yellow");
+    ellipse(0,0,125,175);
+    for(var i = 0; i < 200; i++) {
+    drop[i].show();
+    drop[i].update();
+  }
+  }
 
+  function Drop() {
+    this.x = random(0, width);
+    this.y = random(0, -height);
+
+    this.show = function() {
+      noStroke();
+      fill(255);
+      ellipse(this.x, this.y, random(1, 5), random(1, 5));
+    }
+    this.update = function() {
+      this.speed = random(5, 10);
+      this.gravity = 1.05;
+      this.y = this.y + this.speed*this.gravity;
+
+      if (this.y > height) {
+        this.y = random(0, -height);
+        this.gravity = 0;
+  }
+  }
+  }
 
   if (mouseIsPressed) {
     // stuff that shows when the mouse is pressed
+    function draw() {
+      background(220);
+      fill("brown");
+      rect(200,200,200,100);
+      fill("blue");
+      ellipse(150,250,75,75);
+      ellipse(250,250,75,75);
+      line(400, 50 , 0 , 50);
+      line(400, 300 , 0 , 300);
+      line(400, 150 , 0 , 150);
+      fill ("yellow");
+      ellipse(0,0,125,175);
 
+    }
 
   } else {
     // when the mouse isn't pressed!
