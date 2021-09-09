50a34af614b1842483833c1ae53ca12cc4732ec2
ksnels2 <ksnels2@ilstu.edu>
2021-09-08 19:15:59 -0500

    wrsegf

---
+++ Unit1/background/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+  rectMode(CENTER);
+        }
+
+function draw() {
+  background(220);
+  fill("brown");
+  rect(200,200,200,100);
+  fill("blue");
+  ellipse(150,250,75,75);
+  ellipse(250,250,75,75);
+  line(400, 50 , 0 , 50);
+  line(400, 300 , 0 , 300);
+  line(400, 150 , 0 , 150);
+  fill ("yellow");
+  ellipse(0,0,125,175);
+
+}
