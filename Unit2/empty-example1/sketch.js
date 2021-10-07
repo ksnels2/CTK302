1a40415038f2214de4a63e34bcad9e2d3dc3d4fd
ksnels2 <ksnels2@ilstu.edu>
2021-10-07 10:31:40 -0500

    wouihvfuioWHF

---
+++ Unit2/empty-example1/sketch.js
@@ -0,0 +1,129 @@
+let state = 0;
+let timer = 0;
+let mic;
+let vol;
+let cake;
+
+function setup() {
+  createCanvas(500, 500);
+
+  // code for initializing mic in.
+  mic = new p5.AudioIn(); // what does "new" mean?
+  mic.start();
+
+  cake = loadImage("assets/cake.jpg");
+}
+
+function draw() {
+
+
+  switch (state) {
+    case 0:
+      background('black');
+      image(cake, 200, 200, 150, 150);
+      text("welcome, please click on the cake", 100, 100);
+      break;
+
+    case 1:
+      background('yellow');
+      text("listening", 100, 100);
+      // get the sound input
+      vol = (mic.getLevel().toFixed(2)); // returned level is between 0 and 1
+      if (vol > .1) {
+        state = 2 ;
+      }
+      break;
+
+    case 2:
+      background('red');
+      text("state 2", 100, 100);
+      break;
+
+  }
+
+  fill('white');
+  text(mouseX + ", " + mouseY, 15, 15);
+}
+
+function mouseReleased() {
+  if ((state == 0) && (mouseX > 200) && (mouseX < 350) && (mouseY > 200) && (mouseY < 350)) {
+    state = 1;
+
+  }
+}
+
+function touchStarted() {
+  getAudioContext().resume();
+}
+Rose Marshack — Today at 10:39 AM
+Here is the final code with a bunch of timer states at the end:
+let state = 0;
+let timer = 0;
+let mic;
+let vol;
+let cake;
+
+function setup() {
+  createCanvas(500, 500);
+
+  // code for initializing mic in.
+  mic = new p5.AudioIn(); // what does "new" mean?
+  mic.start();
+
+  cake = loadImage("assets/cake.jpg");
+}
+
+function draw() {
+
+
+  switch (state) {
+    case 0:
+      background('black');
+      image(cake, 200, 200, 150, 150);
+      text("welcome, please click on the cake", 100, 100);
+      break;
+
+    case 1:
+      background('yellow');
+      text("listening", 100, 100);
+      // get the sound input
+      vol = (mic.getLevel().toFixed(2)); // returned level is between 0 and 1
+      if (vol > .1) {
+        state = 2 ;
+      }
+      break;
+
+    case 2:
+      background('red');
+      text("state 2", 100, 100);
+      timer++ ;
+      if (timer > 560) {
+        timer = 0 ;
+        state = 3 ;
+      }
+      break;
+
+      case 3:
+      background('blue') ;
+      timer++ ;
+      if (timer > 860) {
+        timer = 0 ;
+        state = 0 ;
+      }
+
+  }
+
+  fill('white');
+  text(mouseX + ", " + mouseY, 15, 15);
+}
+
+function mouseReleased() {
+  if ((state == 0) && (mouseX > 200) && (mouseX < 350) && (mouseY > 200) && (mouseY < 350)) {
+    state = 1;
+
+  }
+}
+
+function touchStarted() {
+  getAudioContext().resume();
+}
---
+++ Unit2/mp2/sketch.js
@@ -0,0 +1,58 @@
+let state = 0;
+let timer = 0;
+let pizzza,meltCream,iceCream;
+function setup() {
+  createCanvas(500, 500);
+  pizzza =  loadImage("assets/pizzza.jpg");
+  meltCream =  loadImage("assets/meltedicecream.jpg");
+  iceCream =  loadImage("assets/icecreamSunday.jpg");
+    //imageMode(CENTER);
+}
+
+function draw() {
+
+  background(100);
+
+  switch (state) {
+
+    case 0:
+      text("click the square start", 100, 100);
+      fill("red");
+      rect(100, 100, 100, 100);
+      break;
+
+    case 1:
+      text("tap the pizza to eat dessert", 100, 100);
+      image(pizzza,200,200,100,100);
+      if (touches.length==1){
+        state=2;
+      }
+      break;
+
+    case 2:
+      text("the Icecream will melt fast", 100, 100);
+      image(iceCream,200,200,100,100);
+      timer++ ;
+      if (timer > 360) {
+        timer = 0 ;
+        state = 3 ;
+      }
+      break;
+      case 3:
+      text('melted Icecream',100,100) ;
+      image(meltCream,200,200,100,100);
+      timer++ ;
+      if (timer > 360) {
+        timer = 0 ;
+        state = 0 ;
+      }
+  }
+//text("touches = "+touches[], 300, 300) ;
+}
+
+function mouseReleased() {
+  if ((state == 0) && (mouseX > 100) && (mouseX < 200) && (mouseY > 100) && (mouseY < 200)) {
+
+    state = 1;
+  }
+}
